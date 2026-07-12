aacf01d0fbebcbedf3ac766bf53cfb61ac04dcf6
20242275-jpg <20242275@aloe.ulima.edu.com>
2026-07-12 14:38:21 -0500

    Lo devolví a como estaba al inicio (Tercer Commit)

--- src/sistemainmobiliaria/poo/SistemaInmobiliariaPOO.java
+++ src/sistemainmobiliaria/poo/SistemaInmobiliariaPOO.java
@@ -14,7 +14,7 @@ public class SistemaInmobiliariaPOO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Prueba para Github (Segundo Commit).");
+        // TODO code application logic here
     }
     
 }
